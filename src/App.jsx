c10319a0898cbaaea716a9fa799f18f230b3a6bb
Tariqul-22 <tariqul.abf@gmail.com>
2024-03-23 16:05:53 +0600

    setup tailwind and daisyui

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+  
+      <h1 className='text-4xl bg-green-400'>Vite + React</h1>
+      <img className="mask mask-heart" src="https://daisyui.com/images/stock/photo-1567653418876-5bb0e566e1c2.jpg" />
+      
+    </>
+  )
+}
+
+export default App
